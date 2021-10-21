295ef6804ecaebf63d4dd8c086da76a8a84e943c
„Laimonas <laimonas.lukosevicius@gmail.com>
2021-10-21 11:58:28 +0300

    Create 64 random colors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+const width = 8;
+const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
 
-import './App.css';
+const App = () => {
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+  const createBoard = () => {
+    const randomColorArrangement = []
+
+    for (let i = 0; i < width * width; i++) {
+      const randomColor =
+        candyColors[Math.floor(Math.random() * candyColors.length)];
+      randomColorArrangement.push(randomColor);
+    }
+    console.log(randomColorArrangement);
+  };
+  createBoard();
+
+
+  return <div></div>;
+};
 
 export default App;
