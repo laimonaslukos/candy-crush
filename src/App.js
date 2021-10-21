ca5095348bbe64f35ffedaeeaea2217b7403cf51
„Laimonas <laimonas.lukosevicius@gmail.com>
2021-10-21 13:10:09 +0300

    Add checkForColumnOfFour, checkForRowOfFour, checkForColumnOfThree, checkForRowOfThree,

--- src/App.js
+++ src/App.js
@@ -6,6 +6,58 @@ const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
 const App = () => {
   const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
 
+
+  const checkForColumnOfFour = () => {
+    for(let i = 0; i < 39; i++){
+      const columnOfFour = [i, i +width, i + width * 2, i * width * 3]
+      const desidedColor = currentColorArrangement[i]
+  
+      if( columnOfFour.every(square => currentColorArrangement[square] === desidedColor)){
+        columnOfFour.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
+
+  const checkForRowOfFour = () => {
+    for(let i = 0; i < 64; i++){
+      const rowOfFour = [i, i + 1, i + 2, i + 3]
+      const desidedColor = currentColorArrangement[i]
+      const notValid = [ 5, 6, 7, 13, 14, 15, 22, 23, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
+      if(notValid.includes(i)) continue
+  
+      if( rowOfFour.every(square => currentColorArrangement[square] === desidedColor)){
+        rowOfFour.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
+const checkForColumnOfThree = () => {
+  for(let i = 0; i < 47; i++){
+    const columnOfThree = [i, i +width, i + width * 2]
+    const desidedColor = currentColorArrangement[i]
+
+    if( columnOfThree.every(square => currentColorArrangement[square] === desidedColor)){
+      columnOfThree.forEach(square => currentColorArrangement[square] = '')
+    }
+  }
+}
+
+const checkForRowOfThree = () => {
+  for(let i = 0; i < 64; i++){
+    const rowOfThree = [i, i + 1, i + 2]
+    const desidedColor = currentColorArrangement[i]
+    const notValid = [ 6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+    if(notValid.includes(i)) continue
+
+    if( rowOfThree.every(square => currentColorArrangement[square] === desidedColor)){
+      rowOfThree.forEach(square => currentColorArrangement[square] = '')
+    }
+  }
+}
+
+
+
   const createBoard = () => {
     const randomColorArrangement = [];
 
@@ -21,6 +73,20 @@ const App = () => {
     createBoard();
   }, []);
 
+useEffect (() => {
+
+  const timer = setInterval(()=> {
+checkForColumnOfFour()
+checkForRowOfFour()
+checkForColumnOfThree()
+checkForRowOfThree()
+
+setCurrentColorArrangement([...currentColorArrangement])
+  }, 100)
+  return() => clearInterval(timer)
+  
+}, [checkForColumnOfFour, checkForRowOfFour, checkForColumnOfThree, checkForRowOfThree, currentColorArrangement])
+
   console.log(currentColorArrangement);
 
   return <div className="app">
@@ -29,6 +95,7 @@ const App = () => {
   <img 
   key={index}
   style={{backgroundColor: candyColor}} 
+  alt={candyColor}
   />
 ))}
 </div>
